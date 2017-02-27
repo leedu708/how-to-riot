043f65275e3a1651a3522c80cdf0d8bbf85aab64
Dustin Lee <leedu708@gmail.com>
2017-02-27 10:02:38 -0600

    init angular app

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var howTo = angular.module('howTo', ['ui.router', 'ngAnimate', 'ngSanitize']);
+
+howTo.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/home');
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'views/home.html'
+    })
+})
